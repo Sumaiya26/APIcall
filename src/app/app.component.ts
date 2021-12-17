47784225fa4bdb6ae412a327c34e36955e13978f
Sumaiya26 <summisyed7878@gmail.com>
2021-12-17 19:56:49 +0530

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { UsersService } from './users.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CallAPI';
+  datas:any = []
+
+  constructor(private user:UsersService){
+    this.user.getData().subscribe(data =>{
+      console.warn(data)
+      this.datas = data
+    })
+  }
+}
